4714db7b35401482695d7b9810596fb54bffed17
Ronald Huereca <ronald@mediaron.com>
2019-07-08 01:09:55 -0500

    Default ID is 0.

--- js/social-networks.js
+++ js/social-networks.js
@@ -7,7 +7,7 @@ jQuery( document ).ready( function( $ ) {
 		var $option  = $( '#user-profile-enhanced-social-options option:selected' );
 		var $options = $( '.user-profile-enhanced-social-item' );
 		var order = $options.length;
-		var html     = '<div class="user-profile-enhanced-social-item" data-order="' + order + '" data-icon="' + $option.data( 'icon' ) + '"><i class="' + $option.data( 'icon' ) + '"></i> <input class="regular-text" type="text" value="" placeholder="' + upp_enhanced.placeholder + '" /> <a class="user-profile-enhanced-social-item-save button button-secondary" href="#" class="button button-secondary">' + upp_enhanced.save + '</a> <a class="user-profile-enhanced-social-item-remove button button-secondary button-link-delete" href="#" class="button button-secondary">' + upp_enhanced.remove + '</a></div>';
+		var html     = '<div class="user-profile-enhanced-social-item" data-order="' + order + '" data-icon="' + $option.data( 'icon' ) + '" data-id="0"><i class="' + $option.data( 'icon' ) + '"></i> <input class="regular-text" type="text" value="" placeholder="' + upp_enhanced.placeholder + '" /> <a class="user-profile-enhanced-social-item-save button button-secondary" href="#" class="button button-secondary">' + upp_enhanced.save + '</a> <a class="user-profile-enhanced-social-item-remove button button-secondary button-link-delete" href="#" class="button button-secondary">' + upp_enhanced.remove + '</a></div>';
 
 		$( '#user-profile-picture-enhanced-social-networks' ).append( html ).find( 'input:last' ).focus();
 	} );
