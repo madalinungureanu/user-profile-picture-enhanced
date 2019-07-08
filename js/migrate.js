b78c653fe76b6a6bf8998f649b1c289c69839bd8
Ronald Huereca <ronald@mediaron.com>
2019-07-07 19:02:56 -0500

    Adding migration option for users.

--- js/migrate.js
+++ js/migrate.js
@@ -1,3 +1,28 @@
 jQuery( document ).ready( function( $ ) {
-	alert('yo');
+	var nonce  = $( '#_upp_migrate_posts' ).val();
+	var offset = 0;
+	var action = 'migrate_upp_data';
+	$( '#upp-migrate' ).on( 'click', function( e ) {
+		e.preventDefault();
+		$( this ).attr( 'disabled', 'disabled' ).val( upp_enhanced.migrating );
+		mpp_ajax_callback( {
+			action: action,
+			offset: offset,
+			nonce: nonce
+		} );
+	} );
+
+	function mpp_ajax_callback( args ) {
+		$.post( ajaxurl, args, function( response ) {
+			if ( response.more_posts ) {
+				mpp_ajax_callback( {
+					action: action,
+					offset: response.offset,
+					nonce: nonce
+				} );
+			} else {
+				$('#upp-migrate').val( upp_enhanced.migrated );
+			}
+		}, 'json' );
+	}
 } );
--- php/admin/class-migrate.php
+++ php/admin/class-migrate.php
@@ -28,6 +28,53 @@ class Migrate {
 		if ( false === $options['migrated'] ) {
 			add_action( 'admin_notices', array( $this, 'show_migration_notice' ) );
 			add_action( 'admin_menu', array( $this, 'register_admin_page' ) );
+			add_action( 'wp_ajax_migrate_upp_data', array( $this, 'ajax_migrate_data' ) );
+		}
+	}
+
+	/**
+	 * Migrates User Profile Data to the new format
+	 */
+	public function ajax_migrate_data() {
+		$post_data = wp_unslash( $_POST );
+		if ( ! current_user_can( 'edit_pages' ) || ! wp_verify_nonce( $post_data['nonce'], 'upp-migrate-posts' ) ) {
+			die( '' );
+		}
+
+		$offset = absint( $post_data['offset'] );
+
+		$post_args = array(
+			'posts_per_page' => 10,
+			'offset'         => $offset,
+			'post_type'      => 'mt_pp',
+			'orderby'        => 'date',
+			'order'          => 'DESC',
+		);
+		$posts     = get_posts( $post_args );
+		if ( ! empty( $posts ) ) {
+			foreach ( $posts as $post ) {
+				$user_id = $post->post_author;
+				$user    = get_user_by( 'id', $user_id );
+
+				// Update the post title.
+				$post->post_title = esc_html( $user->data->display_name );
+				wp_update_post( $post );
+				$offset++;
+			}
+			$return               = array();
+			$return['offset']     = $offset;
+			$return['more_posts'] = true;
+			die( wp_json_encode( $return ) );
+		} else {
+			$return['offset']     = $offset;
+			$return['more_posts'] = false;
+
+			// Update option to show we have migrated.
+			global $mt_pp;
+			$options             = $mt_pp->get_options();
+			$options['migrated'] = true;
+			$mt_pp->update_options( $options );
+			die( wp_json_encode( $return ) );
 		}
 	}
 
@@ -81,6 +128,14 @@ class Migrate {
 			array( 'jquery' ),
 			USER_PROFILE_PICTURE_ENHANCED_VERSION,
 			true
+		);
+		wp_localize_script(
+			'upp-enhanced-migrate',
+			'upp_enhanced',
+			array(
+				'migrating' => __( 'Migrating...', 'user-profile-picture-enhanced' ),
+				'migrated'  => __( 'All Done!', 'user-profile-profile-enhanced' ),
+			)
 		);
 	}
 }
