f65f7debd2dacfa51db6dc3583234692a7689087
Ronald Huereca <ronald@mediaron.com>
2019-07-10 13:58:22 -0500

    Getting the front-end output.

--- php/blocks/class-avatar.php
+++ php/blocks/class-avatar.php
@@ -148,9 +148,57 @@ class Avatar {
 		if ( is_admin() ) {
 			return;
 		}
-		$html  = '<div class=aligncenter">';
-		$html .= $attributes['html'];
-		$html .= '</div>';
-		return $html;
+
+		// Loading Attributes.
+		$align = 'center';
+		if ( isset( $attributes['align'] ) ) {
+			$align = $attributes['align'];
+		}
+		$image_url         = $attributes['imgUrl'];
+		$alt               = $attributes['alt'];
+		$width             = $attributes['width'];
+		$height            = $attributes['height'];
+		$bg_img_parallax   = $attributes['bgImgParallax'];
+		$bg_img_fill       = $attributes['bgImgFill'];
+		$bg_img            = $attributes['bgImg'];
+		$caption_font_size = $attributes['captionFontSize'];
+		$caption_color     = $attributes['captionColor'];
+		$caption           = $attributes['caption'];
+		$border_radius     = $attributes['borderRadius'];
+		$border            = $attributes['border'];
+		$border_color      = $attributes['borderColor'];
+		$img_bg_color      = $attributes['imgBgColor'];
+		$img_padding       = $attributes['imgPadding'];
+		$img_border_color  = $attributes['imgBorderColor'];
+		$img_border        = $attributes['imgBorder'];
+		$padding           = $attributes['padding'];
+		$avatar_shape      = $attributes['avatarShape'];
+		$background_color  = $attributes['backgroundColor'];
+
+		ob_start();
+		?>
+		<div
+			class="upp-enhanced-avatar <?php echo esc_attr( $avatar_shape ); ?> align<?php echo esc_attr( $align ); ?>"
+			style="background-color: <?php echo esc_attr( $background_color ); ?>; padding: <?php echo absint( $padding ); ?>px; border: <?php echo absint( $border ); ?>px solid <?php echo esc_attr( $border_color ); ?>; border-radius: <?php echo absint( $border_radius ); ?>px; <?php echo ( ! empty( $bg_img ) ) ? sprintf( 'background-image: url(%s);', esc_url( $bg_img ) ) : ''; ?> background-size: <?php echo esc_attr( $bg_img_fill ); ?>; background-attachment: <?php echo $bg_img_parallax ? 'fixed' : 'inherit'; ?>"
+		>
+			<img
+			src="<?php echo esc_attr( $image_url ); ?>"
+			alt="<?php echo esc_attr( $alt ); ?>"
+			width="<?php echo absint( $width ); ?>"
+			height="<?php echo absint( $height ); ?>"
+			style="background-color: <?php echo esc_attr( $img_bg_color ); ?>; border: <?php echo esc_attr( $img_border ); ?>px solid <?php echo esc_attr( $img_border_color ); ?>; padding: <?php echo esc_attr( $img_padding ); ?>px;"
+			/>
+			<?php
+			if ( ! empty( $caption ) ) {
+				?>
+				<h2 class="upp-enhanced-avatar-caption" style="color: <?php echo esc_attr( $caption_color ); ?>; font-size: <?php echo esc_attr( $caption_font_size ); ?>px;">
+					<?php echo wp_kses_post( $caption ); ?>
+				</h2>
+				<?php
+			}
+			?>
+		</div>
+		<?php
+		return ob_get_clean();
 	}
 }
