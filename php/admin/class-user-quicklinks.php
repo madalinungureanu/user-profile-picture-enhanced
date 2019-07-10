62d29d5940a236cab4ff30c8230fd04be15d0d82
Ronald Huereca <ronald@mediaron.com>
2019-07-10 18:49:53 -0500

    Adding quick link to edit and view profile page.

--- php/admin/class-user-quicklinks.php
+++ php/admin/class-user-quicklinks.php
@@ -36,6 +36,36 @@ class User_Quicklinks {
 			return;
 		}
 		add_filter( 'user_row_actions', array( $this, 'insert_user_profile_quicklink' ), 10, 2 );
+		add_action( 'mpp_user_profile_form', array( $this, 'add_quicklinks_to_profile_page' ), 20 );
+	}
+
+	/**
+	 * Add a quick link to the profile page.
+	 */
+	public function add_quicklinks_to_profile_page() {
+		$profile_post_id = absint( get_user_option( 'metronet_post_id', $user->data->ID ) );
+		if ( $profile_post_id ) {
+			$user_edit_page = add_query_arg(
+				array(
+					'post'   => $profile_post_id,
+					'action' => 'edit',
+				),
+				admin_url( 'post.php' )
+			);
+			$permalink      = get_permalink( $profile_post_id );
+			if ( $permalink ) {
+				?>
+				<tr valign="top">
+					<th scope="row"><?php esc_html_e( 'Public Profile Page', 'user-profile-picture-enhanced' ); ?></th>
+					<td id="user-profile-picture-enhanced-view-profile-page">
+						<p>
+							<?php echo esc_html__( 'Profile pages for your users are public.' ); ?> | <a href="<?php echo esc_url( $user_edit_page ); ?>" target="_blank"><?php echo esc_html__( 'Edit Profile Page', 'user-profile-picture-enhanced' ); ?></a> | <a href="<?php echo esc_url( $permalink ); ?>" target="_blank"><?php echo esc_html( 'View Profile Page', 'user-profile-picture-enhanced' ); ?></a>
+						</p>
+					</td>
+				</tr>
+				<?php
+			}
+		}
 	}
 
 	/**
