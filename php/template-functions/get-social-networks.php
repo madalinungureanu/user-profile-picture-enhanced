4591e8ab8d40f02227a1d07219cdbf2afe0a469b
Ronald Huereca <ronald@mediaron.com>
2019-07-12 17:07:20 -0500

    Adding beginning of template function for retrieving social networks.

---
+++ php/template-functions/get-social-networks.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template function for retrieving social media icons for a user.
+ *
+ * @package   User_Profile_Picture_Enhanced
+ */
+
+/**
+ * Get a list of social networks for the user
+ *
+ * @param int    $user_id The User ID to retrieve the social networks for.
+ * @param string $context The context for the user output. Context can be raw or output.
+ *
+ * @return mixed Array or string depending on the context.
+ */
+function uppe_get_social_networks( $user_id, $context = 'output' ) {
+	return '';
+}
