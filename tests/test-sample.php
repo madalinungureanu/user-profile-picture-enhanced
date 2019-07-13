6bc369e5ff3a85734a2e8426611ab55414bfdca1
Ronald Huereca <ronald@mediaron.com>
2019-07-13 05:52:20 -0500

    Adding basic unit tests.

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package User_Profile_Picture_Enhanced
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
