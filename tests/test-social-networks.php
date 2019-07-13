db9c260cef8b381e0023392cdfe447f2d69cf8bb
Ronald Huereca <ronald@mediaron.com>
2019-07-13 06:54:44 -0500

    Adding basic unit tests.

---
+++ tests/test-social-networks.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Class UPP_Enhanced
+ *
+ * @package User_Profile_Picture_Enhanced
+ */
+
+/**
+ * Sample test case.
+ */
+class UPP_Enhanced extends WP_UnitTestCase {
+
+	/**
+	 * A test for getting the social networks for a user.
+	 */
+	public function test_get_social_networks() {
+		// Test social network results
+
+		$this->assertEquals( array(), uppe_get_social_networks( 0, 'raw' ) );
+		$this->assertEquals( '', uppe_get_social_networks( 0, 'output' ) );
+	}
+}
